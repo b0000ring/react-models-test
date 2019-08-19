e2db5bbe0fb26c668306af9fa67cfe628336b998
Aleksander Chirkin <booooring@yahoo.com>
2019-08-20 01:32:47 +0300

    redux added

---
+++ src/store/index.js
@@ -0,0 +1,15 @@
+import { createStore, combineReducers, applyMiddleware } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+// mean interface but it is reserved word
+import components from './reducers/components'
+import todo from './reducers/todo'
+
+const rootReducer = combineReducers({
+    components: components,
+    todo: todo,
+})
+
+const store = createStore(rootReducer, composeWithDevTools())
+
+export default store
